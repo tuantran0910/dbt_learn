3b844553edf32009847ea9417226cd56abd10062
Trần Ngọc Tuấn <tntuan0910@gmail.com>
2024-10-31 02:22:45 +0000

    add: first dbt models

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,42 @@
+WITH customers AS (
+
+    SELECT *
+    FROM {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
+orders AS (
+    
+    SELECT *
+    FROM {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+customer_orders AS (
+
+    SELECT
+        customer_id,
+
+        min(order_date) AS first_order_date,
+        max(order_date) AS most_recent_order_date,
+        count(order_id) AS number_of_orders
+    FROM orders
+    GROUP BY 1    
+
+),
+
+final AS (
+
+    SELECT
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) AS number_of_orders
+    FROM customers
+    LEFT JOIN customer_orders USING (customer_id)
+
+)
+
+SELECT * FROM final
---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,5 @@
+SELECT
+    id AS customer_id,
+    first_name,
+    last_name
+FROM raw.jaffle_shop.customers
